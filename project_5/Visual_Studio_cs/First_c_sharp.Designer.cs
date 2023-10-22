5c87cd60ebed47df10b53f47c1017739a2500e12
Ahmadreza <AHMADREZAGHOLIZADE304@GMAIL.com>
2023-10-23 00:37:41 +0330

    Adding Project 5

---
+++ project_5/Visual_Studio_cs/First_c_sharp.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Project5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Y1 = new System.Windows.Forms.TextBox();
+            this.X2 = new System.Windows.Forms.TextBox();
+            this.Y2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // X1
+            // 
+            this.X1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X1.Location = new System.Drawing.Point(85, 20);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(335, 38);
+            this.X1.TabIndex = 0;
+            this.X1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Y1
+            // 
+            this.Y1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Y1.Location = new System.Drawing.Point(85, 64);
+            this.Y1.Name = "Y1";
+            this.Y1.Size = new System.Drawing.Size(335, 38);
+            this.Y1.TabIndex = 2;
+            this.Y1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // X2
+            // 
+            this.X2.AccessibleName = "";
+            this.X2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X2.ForeColor = System.Drawing.Color.Black;
+            this.X2.Location = new System.Drawing.Point(85, 131);
+            this.X2.Name = "X2";
+            this.X2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.X2.Size = new System.Drawing.Size(335, 38);
+            this.X2.TabIndex = 3;
+            this.X2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.X2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Y2
+            // 
+            this.Y2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Y2.Location = new System.Drawing.Point(85, 175);
+            this.Y2.Name = "Y2";
+            this.Y2.Size = new System.Drawing.Size(335, 38);
+            this.Y2.TabIndex = 4;
+            this.Y2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(417, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Distance Calculator";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(22, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(22, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 36);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(22, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 36);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Y1:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(448, 306);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Y2);
+            this.Controls.Add(this.X2);
+            this.Controls.Add(this.Y1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.X1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox X1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Y1;
+        private System.Windows.Forms.TextBox X2;
+        private System.Windows.Forms.TextBox Y2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ project_5/Visual_Studio_cs/First_c_sharp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            X2.TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((X1.Text == "") || (Y1.Text == "") || (X2.Text == "") || (Y2.Text == ""))
+            {
+                MessageBox.Show("Invalid Input!");
+            }
+            else if ((X1.Text.All(c => Char.IsDigit(c) || c == '.')) && (X1.Text.All(c => Char.IsDigit(c) || c == '.')) && (X2.Text.All(c => Char.IsDigit(c) || c == '.')) && (Y2.Text.All(c => Char.IsDigit(c) || c == '.')))
+            {
+                double deltaX = double.Parse(X2.Text) - double.Parse(X1.Text);
+                double deltaY = double.Parse(Y2.Text) - double.Parse(Y1.Text);
+
+                double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+                MessageBox.Show("Distance = " + distance.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Inputs aren't Digits!");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
