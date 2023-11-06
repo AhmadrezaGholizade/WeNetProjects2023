195b87c9517b9973964b41502fd0421294cb5aa1
Ahmadreza <AHMADREZAGHOLIZADE304@GMAIL.com>
2023-11-06 18:52:56 +0330

    Adding Project 9

---
+++ Project_9/Project_9/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Project_9
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "GetNearestAPI",
+                routeTemplate: "api/{controller}/{action}/{x}/{y}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Project_9/Project_9/Models/architectural_firms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_9.Models
+{
+    public class architectural_firms
+    {
+        // Firm's Attributes
+        public int ID { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Name { get; set; }
+        public string Founder { get; set; }
+        public string Country { get; set; }
+        public List<string> Projects { get; set; }
+        public string ContactInfo { get; set; }
+    }
+}
